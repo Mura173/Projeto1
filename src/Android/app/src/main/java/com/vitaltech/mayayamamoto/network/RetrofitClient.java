4135b3cae488dfa166fbe9360502be2db0aa5782
Murilo Ângelo <murapb07@gmail.com>
2026-05-04 15:04:40 -0300

    {

--- src/Android/app/src/main/java/com/vitaltech/mayayamamoto/network/RetrofitClient.java
+++ src/Android/app/src/main/java/com/vitaltech/mayayamamoto/network/RetrofitClient.java
@@ -6,12 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
 
     // Endereco do servidor
-
-    // IP padrao para utilizar no EMULADOR do Android Studio
-    private static final String BASE_URL = "http://10.0.2.2:5000/";
-
-    //IP do computador
-    //private static final String BASE_URL = "http://xxx.xxx.x.x:5000/";
+    private static final String BASE_URL = "https://back-end-vitaltech.onrender.com";
 
     // Instancia unica do Retrofit
     private static Retrofit retrofit;
